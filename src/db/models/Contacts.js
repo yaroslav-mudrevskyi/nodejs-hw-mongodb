853795f3e03efd62a656c3f2b0762ac13d3306fe
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-10-12 21:49:49 +0300

    finished all tasks

---
+++ src/db/models/Contacts.js
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+const ContactCollection = model('contact', contactSchema);
+
+export default ContactCollection;
--- src/utils/env.js
+++ src/utils/env.js
@@ -4,6 +4,8 @@ export const env = (name, defaultValue) => {
   const value = process.env[name];
 
   if (value) return value;
+
   if (defaultValue) return defaultValue;
+
   throw new Error(`Missing: process.env[${name}]`);
 };
