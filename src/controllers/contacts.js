05e0ac1f5281589a0da23518ce82ec2eb50b3596
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-10-20 23:08:59 +0300

    finished all tasks

---
+++ src/controllers/contacts.js
@@ -0,0 +1,65 @@
+import createHttpError from 'http-errors';
+
+import * as contactServices from '../services/contacts.js';
+
+export const getContactsController = async (req, res) => {
+  const data = await contactServices.getContacts();
+
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data,
+  });
+};
+
+export const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+  const data = await contactServices.getContactById(contactId);
+
+  if (!data) {
+    throw createHttpError(404, `Contact not found`);
+  }
+
+  res.json({
+    status: 200,
+    message: `"Successfully found contact with id ${contactId}!"`,
+    data,
+  });
+};
+
+export const addContactController = async (req, res) => {
+  const data = await contactServices.addContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data,
+  });
+};
+
+export const updateContactController = async (req, res) => {
+  const { contactId } = req.params;
+
+  const result = await contactServices.updateContactById(contactId, req.body);
+
+  if (!result) {
+    throw createHttpError(404, `Movie with id=${contactId} not found`);
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result,
+  });
+};
+
+export const deleteContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const data = await contactServices.deleteContactById(contactId);
+
+  if (!data) {
+    throw createHttpError(404, `Contact not found`);
+  }
+
+  res.status(204).send();
+};
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,3 +3,22 @@ import ContactCollection from '../db/models/Contacts.js';
 export const getContacts = () => ContactCollection.find();
 
 export const getContactById = (id) => ContactCollection.findById(id);
+
+export const addContact = (payload) => ContactCollection.create(payload);
+
+export const updateContactById = async (_id, payload, options = {}) => {
+  const result = await ContactCollection.findOneAndUpdate({ _id }, payload, {
+    new: true,
+    includeResultMetadata: true,
+    ...options,
+  });
+
+  if (!result || !result.value) return null;
+
+  return {
+    data: result.value,
+  };
+};
+
+export const deleteContactById = (_id) =>
+  ContactCollection.findByIdAndDelete({ _id });
