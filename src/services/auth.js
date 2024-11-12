25f82b5492f6570487f057e4af75bf48beaa61e2
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-11-12 14:42:59 +0200

    finished auth register

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,16 @@
 import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
 
 import UserCollection from '../db/models/User.js';
 
 export const registerUser = async (payload) => {
-  const { email } = payload;
+  const { email, password } = payload;
   const user = await UserCollection.findOne({ email });
   if (user) {
     throw createHttpError(409, 'Email already exist!');
   }
 
-  return await UserCollection.create(payload);
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  return await UserCollection.create({ ...payload, password: hashPassword });
 };
